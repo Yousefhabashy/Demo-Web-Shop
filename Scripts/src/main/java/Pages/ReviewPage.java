5a2e60c1ef441d07c4b2434f194b43881599901a
Yousef Havashy <yousefhabashy2004@gmail.com>
2026-01-30 12:13:22 +0200

    Completed pages classes

--- Scripts/src/main/java/Pages/ReviewPage.java
+++ Scripts/src/main/java/Pages/ReviewPage.java
@@ -2,9 +2,57 @@ package Pages;
 
 import Base.PagesBase;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ReviewPage extends PagesBase {
     public ReviewPage(WebDriver driver) {
         super(driver);
     }
+
+    @FindBy(id = "AddProductReview_Title")
+    WebElement reviewTitle;
+    @FindBy(id = "AddProductReview_ReviewText")
+    WebElement reviewText;
+
+    @FindBy(id = "addproductrating_1")
+    WebElement badReviewRate;
+    @FindBy(id = "addproductrating_2")
+    WebElement acceptableReviewRate;
+    @FindBy(id = "addproductrating_3")
+    WebElement goodReviewRate;
+    @FindBy(id = "addproductrating_4")
+    WebElement veryGoodReviewRate;
+    @FindBy(id = "addproductrating_5")
+    WebElement excellentReviewRate;
+    @FindBy(css = "input.button-1.write-product-review-button")
+    WebElement submitReview;
+
+    public void addReview(String title, String review, int rate) {
+
+        setElementText(reviewTitle, title);
+        setElementText(reviewText , review);
+        if (rate <= 1) {
+            waitFor().until(ExpectedConditions.elementToBeClickable(badReviewRate));
+            clickElementJS(badReviewRate);
+        } else if (rate == 2) {
+            waitFor().until(ExpectedConditions.elementToBeClickable(acceptableReviewRate));
+            clickElementJS(acceptableReviewRate);
+        }else if (rate == 3) {
+            waitFor().until(ExpectedConditions.elementToBeClickable(goodReviewRate));
+            clickElementJS(goodReviewRate);
+        }else if (rate == 4) {
+            waitFor().until(ExpectedConditions.elementToBeClickable(veryGoodReviewRate));
+            clickElementJS(veryGoodReviewRate);
+        }else {
+            waitFor().until(ExpectedConditions.elementToBeClickable(excellentReviewRate));
+            clickElementJS(excellentReviewRate);
+        }
+        waitFor().until(ExpectedConditions.elementToBeClickable(submitReview));
+        clickElementJS(submitReview);
+    }
+
+    @FindBy(css = "div.result")
+    public WebElement successMessage;
 }
--- Scripts/src/main/java/Pages/WishlistPage.java
+++ Scripts/src/main/java/Pages/WishlistPage.java
@@ -1,10 +1,77 @@
 package Pages;
 
 import Base.PagesBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
 
 public class WishlistPage extends PagesBase {
     public WishlistPage(WebDriver driver) {
         super(driver);
+    }
+
+    @FindBy(css = "table.cart")
+    public WebElement productsContainer;
+    private List<WebElement> getProducts() {
+        return productsContainer.findElements(By.cssSelector("td.cart-item-row"));
+    }
+    public WishlistPage.WishlistProduct getProduct(int productNumber) {
+        int index = productNumber - 1;
+        List<WebElement> products = getProducts();
+        if(index >= 0 && index < products.size()) {
+            return  new WishlistPage.WishlistProduct(products.get(index));
+        }
+        throw  new IndexOutOfBoundsException("Products index " + index + " not found!");
+    }
+
+    public static class WishlistProduct {
+
+        private WebElement product;
+        public WishlistProduct(WebElement OrderElement) {
+            this.product = OrderElement;
+        }
+
+        public String getProductTitle() {
+            WebElement titleContainer = product.findElement(By.cssSelector("td.product"));
+            return titleContainer.findElement(By.tagName("a")).getText();
+        }
+        public String getProductUnitPrice() {
+            return product.findElement(By.cssSelector("span.product-unit-price")).getText();
+        }
+        public String getProductTotalPrice() {
+            return product.findElement(By.cssSelector("span.product-subtotal")).getText();
+        }
+        public int getProductQuantity() {
+            String qtyText =  product.findElement(By.cssSelector("input.qty-input")).getAttribute("value");
+            assert qtyText != null;
+            return Integer.parseInt(qtyText);
+        }
+        public void updateQuantity(int newQuantity) {
+            WebElement quantityInput =  product.findElement(By.cssSelector("input.qty-input"));
+            quantityInput.clear();
+            setElementText(quantityInput, String.valueOf(newQuantity));
+            WebElement wishlistUpdate = driver.findElement(By.cssSelector("input.button-2.update-wishlist-button"));
+            clickElementJS(wishlistUpdate);
+        }
+        public void removeProduct() {
+            WebElement removeContainer = product.findElement(By.cssSelector("td.remove-from-cart"));
+            WebElement remove = removeContainer.findElement(By.cssSelector("input[name='removefromcart']"));
+            waitFor().until(ExpectedConditions.elementToBeClickable(remove));
+            clickElementJS(remove);
+            WebElement wishlistUpdate = driver.findElement(By.cssSelector("input.button-2.update-cart-button"));
+            clickElementJS(wishlistUpdate);
+        }
+        public void addProductToCart() {
+            WebElement cartContainer = product.findElement(By.cssSelector("td.add-to-cart"));
+            WebElement add = cartContainer.findElement(By.cssSelector("input[name='addtocart']"));
+            waitFor().until(ExpectedConditions.elementToBeClickable(add));
+            clickElementJS(add);
+            WebElement wishlistUpdate = driver.findElement(By.cssSelector("input.button-2.update-wishlist-button"));
+            clickElementJS(wishlistUpdate);
+        }
     }
 }
